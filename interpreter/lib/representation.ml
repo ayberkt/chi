6cf4cbcdcef7762bcabd5378e5534184a8ed593a
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-12-03 22:29:35 +0100

    Clean up the representation generator a bit

--- interpreter/lib/representation.ml
+++ interpreter/lib/representation.ml
@@ -15,7 +15,7 @@ class environment =
     val ccount = ref 0
     val rho : (string, int) H.t = String.Table.create ()
 
-    method add_var (v : variable) : int =
+    method code_var (v : variable) : int =
       match v with
       | Variable x ->
           (match H.find rho x with
@@ -26,27 +26,22 @@ class environment =
                incr vcount;
                vr)
 
-    method add_const (c : constructor) : int =
-      match c with
-      | Constructor x ->
-          (match H.find rho x with
-           | Some n -> n
-           | None ->
-               let cc = !ccount in
-               H.set rho ~key:x ~data:!ccount;
-               incr ccount;
-               cc)
+    method code_const (Constructor x : constructor) : int =
+      match H.find rho x with
+      | Some n -> n
+      | None ->
+          let cc = !ccount in
+          H.set rho ~key:x ~data:!ccount;
+          incr ccount;
+          cc
 
     method explain_coding () : unit =
       let
-        explain ~key:(key : string) ~data:(data : int) : unit =
-          P.printf "%s |-> %d\n" key data; Out_channel.flush Out_channel.stdout;
+        explain ~key:key ~data:data = P.printf "%s |-> %d\n" key data
       in
         P.printf "%s\n" "Using the following encodings";
         H.iteri rho ~f:explain
   end
-
-type code = (variable, int) Map.Poly.t
 
 let rec rep_nat : int -> exp = function
   | 0 -> Const (Constructor "Zero", [])
@@ -54,39 +49,38 @@ let rec rep_nat : int -> exp = function
 
 exception Unbound
 
-let represent (e : exp) (mrho : environment option) =
+let represent (e : exp) (mrho : environment option) (verbose : bool) =
+  let ccons    = Constructor "Cons" in
+  let cnil    = Constructor "Nil"   in
+  let nil_rep = Const (cnil, [])    in
   let rho =
     match mrho with
     | Some rho -> rho
     | None -> new environment
   in let rec rep (e : exp) : exp =
     match e with
-    | Var v -> Const (Constructor "Var", [rep_nat (rho#add_var v)])
+    | Var v -> Const (Constructor "Var", [rep_nat (rho#code_var v)])
     | Lambda (v, e) ->
-        Const (Constructor "Lambda", [rep_nat (rho#add_var v) ; rep e])
+        Const (Constructor "Lambda", [rep_nat (rho#code_var v); rep e])
     | Apply (e1, e2) -> Const (Constructor "Apply", [rep e1; rep e2])
-    | Case (e, bs) -> Const (Constructor "Case", [rep e; represent_bs bs])
+    | Case (e, bs) -> Const (Constructor "Case", [rep e; rep_bs bs])
     | Rec (v, e) ->
-        Const (Constructor "Rec", [rep_nat (rho#add_var v); rep e])
+        Const (Constructor "Rec", [rep_nat (rho#code_var v); rep e])
     | Const (c, es) ->
-      Const (Constructor "Const",
-             [ rep_nat (rho#add_const c);  represent_multi es ])
-  and represent_multi (es : exp list) : exp =
-    match es with
-    | [] -> Const (Constructor "Nil", [])
-    | e::es -> Const (Constructor "Cons", [rep e; represent_multi es])
-  and represent_bs (bs : br list) : exp =
-    match bs with
-    | [] -> Const (Constructor "Nil", [])
-    | b::bs -> Const (Constructor "Cons", [rep_branch b; represent_bs bs])
+        Const (Constructor "Const",
+              [ rep_nat (rho#code_const c);  represent_multi' es ])
+  and represent_multi' es : exp =
+    L.fold_right es ~f:(fun x y -> Const (ccons, [rep x; y])) ~init:nil_rep
+  and rep_bs (bs : br list) : exp =
+    L.fold_right bs ~f:(fun x r -> Const (ccons, [rep_branch x; r])) ~init:nil_rep
   and rep_vs : variable list -> exp = function
     | [] -> Const (Constructor "Nil", [])
     | v::xs ->
-        Const (Constructor "Cons", [rep_nat (rho#add_var v); rep_vs xs])
+        Const (ccons, [rep_nat (rho#code_var v); rep_vs xs])
   and rep_branch : br -> exp = function
     | Branch (c, xs, e) ->
-        Const (Constructor "Branch", [rep_nat (rho#add_const c); rep_vs xs; rep e])
+        Const (Constructor "Branch", [rep_nat (rho#code_const c); rep_vs xs; rep e])
   in
     let e_rep = rep e in
-    rho#explain_coding ();
+    if verbose then rho#explain_coding () else ();
     (e_rep, rho)
