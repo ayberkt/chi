d852e95d811e0d44032e47b20d1c4d607ab35a13
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-12-03 21:11:56 +0100

    Run the self-interpreter tests in the outer test module

--- test/run_tests.ml
+++ test/run_tests.ml
@@ -1,11 +1,8 @@
-(*
-   Run all the OCaml test suites defined in the project.
-*)
-
 let test_suites: unit Alcotest.test list = [
   "Closed-checking", Test_interpreter.A.tests;
   "Substitution",    Test_interpreter.A.subst_tests;
   "Evaluation",      Test_interpreter.A.eval_tests;
-];;
+  "Self-interpreter", Test_interpreter.A.self_interpreter_tests;
+]
 
-let () = Alcotest.run "chi" test_suites;;
+let () = Alcotest.run "chi" test_suites
