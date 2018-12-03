b7092df057c97bb039c68777ec1468fa4fd766d7
Ayberk Tosun <ayberk.tosun@gmail.com>
2018-12-03 17:02:55 +0100

    Improve the representation-generation

--- interpreter/lib/representation.ml
+++ interpreter/lib/representation.ml
@@ -8,67 +8,71 @@ module H = Hashtbl
 
 exception Todo
 
+class environment =
+  object (_)
+    val vcount = ref 0
+    val ccount = ref 0
+    val rho    = String.Table.create ()
+
+    method add_var (v : variable) : int =
+      match v with
+      | Variable x ->
+          (match H.find rho x with
+           | Some n -> n
+           | None ->
+               let vr = !vcount in
+               H.set rho ~key:x ~data:!vcount;
+               incr vcount;
+               vr)
+
+    method add_const (c : constructor) : int =
+      match c with
+      | Constructor x ->
+          (match H.find rho x with
+           | Some n -> n
+           | None ->
+               let cc = !ccount in
+               H.set rho ~key:x ~data:!ccount;
+               incr ccount;
+               cc)
+  end
+
 type code = (variable, int) Map.Poly.t
 
-let rec represent : int -> exp = function
+let rec rep_nat : int -> exp = function
   | 0 -> Const (Constructor "Zero", [])
-  | n -> Const (Constructor "Suc", [represent n])
+  | n -> Const (Constructor "Suc", [rep_nat (n-1)])
+
+exception Unbound
 
 let represent : exp -> exp =
-  let vcount = ref 0 in
-  let ccount = ref 0 in
-  let rho    = String.Table.create () ~size:20 in
-  let rec represent' (e : exp) : exp =
+  let rho    = new environment in
+  let rec rep (e : exp) : exp =
     match e with
-    | Var (Variable x) ->
-        (match H.find rho x with
-         | Some n -> Const (Constructor "Var", [represent n])
-         | None -> raise Todo)
-    | Lambda (Variable x, e) ->
-        H.set rho ~key:x ~data:!vcount;
-        incr vcount;
-        Const (Constructor "Lambda", [represent' e])
-    | Apply (e1, e2) -> Apply (represent' e1, represent' e2)
-    | Case (e, bs) -> Const (Constructor "Case", [represent' e; represent_bs bs])
-    | Rec (Variable x, e) ->
-        H.set rho ~key:x ~data:!vcount;
-        incr vcount;
-        Const (Constructor "Rec", [represent' e])
-    | Const (Constructor c, es) ->
-        (match H.find rho c with
-         | Some cr -> Const (Constructor "Const", [represent cr; represent' e])
-         | None ->
-             let cr = !ccount in
-             H.set rho ~key:c ~data:!ccount;
-             incr ccount;
-             Const (Constructor "Const", [represent cr; represent_multi es]))
+    | Var v -> Const (Constructor "Var", [rep_nat (rho#add_var v)])
+    | Lambda (v, e) ->
+        Const (Constructor "Lambda", [rep_nat (rho#add_var v) ; rep e])
+    | Apply (e1, e2) -> Const (Constructor "Apply", [rep e1; rep e2])
+    | Case (e, bs) -> Const (Constructor "Case", [rep e; represent_bs bs])
+    | Rec (v, e) ->
+        Const (Constructor "Rec", [rep_nat (rho#add_var v); rep e])
+    | Const (c, es) ->
+      Const (Constructor "Const",
+             [ rep_nat (rho#add_const c);  represent_multi es ])
   and represent_multi (es : exp list) : exp =
     match es with
     | [] -> Const (Constructor "Nil", [])
-    | e::es -> Const (Constructor "Cons", [represent' e; represent_multi es])
+    | e::es -> Const (Constructor "Cons", [rep e; represent_multi es])
   and represent_bs (bs : br list) : exp =
     match bs with
     | [] -> Const (Constructor "Nil", [])
     | b::bs -> Const (Constructor "Cons", [rep_branch b; represent_bs bs])
   and rep_vs : variable list -> exp = function
     | [] -> Const (Constructor "Nil", [])
-    | (Variable x)::xs ->
-        match H.find rho x with
-          | Some n -> Const (Constructor "Cons", [represent n; rep_vs xs])
-        | None ->
-            let vc = !vcount in
-            H.set rho ~key:x ~data:!vcount;
-            incr vcount;
-            Const (Constructor "Cons", [represent vc; rep_vs xs])
+    | v::xs ->
+        Const (Constructor "Cons", [rep_nat (rho#add_var v); rep_vs xs])
   and rep_branch : br -> exp = function
-    | Branch (Constructor c, xs, e) ->
-        match H.find rho c with
-          | Some n ->
-              Const (Constructor "Branch", [represent n; rep_vs xs; represent' e])
-          | None ->
-              let cc = !ccount in
-              H.set rho ~key:c ~data:!ccount;
-              incr ccount;
-              Const (Constructor "Branch", [represent cc; rep_vs xs; represent' e])
+    | Branch (c, xs, e) ->
+        Const (Constructor "Branch", [rep_nat (rho#add_const c); rep_vs xs; rep e])
   in
-    represent'
+    rep
